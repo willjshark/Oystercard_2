115d09414c49bd1d2631d5d1d859f8f36d74115d
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-25 17:23:12 +0100

    complete challenge #05:enable top-up functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,13 @@ class Oystercard
 
 attr_reader :balance
 
-def initialize 
-  @balance = 0
-end
+  def initialize 
+    @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += amount
+  end
+
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../lib/oystercard'
 
 describe Oystercard do 
-it "It had a balance of zero" do
-  expect(subject.balance).to eq(0)
+ 
+  it "it had a balance of zero" do
+    expect(subject.balance).to eq(0)
   end
+ 
+  it "it top up the card with an amount" do
+    # expect(subject).to respond_to(:top_up).with(1).argument
+    # expect(subject.top_up(10)).to eq 10
+    expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+  end
+
 end
