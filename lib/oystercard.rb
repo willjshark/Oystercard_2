95c13b0b5f611ba3acdcbca96645868db76224a6
Courtney Stow <courtney@Courtneys-MacBook-Air.local>
2021-10-26 14:35:52 +0100

    Code changed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,17 @@
-class Oystercard
+# frozen_string_literal: true
 
+class Oystercard
   MAXIMUM_BALANCE = 90
 
-attr_reader :balance
+  attr_reader :balance
 
-  def initialize 
+  def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    fail "maximum balance exceeded" if amount + balance > MAXIMUM_BALANCE
+    raise 'maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
+
     @balance += amount
   end
-
 end
-
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,21 +1,21 @@
-require_relative '../lib/oystercard'
+# frozen_string_literal: true
 
-describe Oystercard do 
- 
-  it "it had a balance of zero" do
+require "oystercard"
+
+describe Oystercard do
+  it 'it had a balance of zero' do
     expect(subject.balance).to eq(0)
   end
- 
-  it "it top up the card with an amount" do
-    # expect(subject).to respond_to(:top_up).with(1).argument
+
+  it 'it top up the card with an amount' do
+    expect(subject).to respond_to(:top_up).with(1).argument
     # expect(subject.top_up(10)).to eq 10
-    expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+    # expect { subject.top_up 10 }.to change { subject.balance }.by 10
   end
 
-  it "To raise error if maximum balance is exceeded" do
+  it 'raises error if the maximum balance is exceeded' do
     maximum_balance = Oystercard::MAXIMUM_BALANCE
     subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error "maximum balance exceeded"
-    end
-
+    expect{ subject.top_up 1 }.to raise_error "maximum balance of #{maximum_balance} exceeded"
+  end
 end
